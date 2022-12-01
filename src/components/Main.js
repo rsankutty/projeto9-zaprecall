49e476bd496ff1d4f0b0fe805805e963837125b5
rsankutty <r.sankutty@gmail.com>
2022-12-01 12:43:48 -0300

    renderizacao carta flipada + componentizacao footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,18 @@ import cards from "./Cards";
 
 import QuestionCard from "./QuestionCard";
 import OpenCard from "./OpenCard";
+import FlippedCard from "./FlippedCard";
+import Footer from "./Footer";
 
 export default function Main() {
   return (
     <ScreenContainer>
-
       <LogoContainer>
         <img src={logo} alt="logo"></img>
         <h1>ZapRecall</h1>
       </LogoContainer>
 
-      {cards.map((item,index) => (
+      {cards.map((item, index) => (
         <QuestionCard key={index + 1} questionIndex={index + 1} />
       ))}
 
@@ -22,15 +23,11 @@ export default function Main() {
         <OpenCard key={index + 1} question={item.question} />
       ))}
 
-      <PerguntaAberta>
-        um monte de bosta
-        <ContainerBotoes>
-          <button>teste</button>
-          <button>merda</button>
-          <button>bosta fadfa </button>
-        </ContainerBotoes>
-      </PerguntaAberta>
-      <FooterConcluidos>um monte de bosta</FooterConcluidos>
+      {cards.map((item, index) => (
+        <FlippedCard key={index + 1} answer={item.answer} />
+      ))}
+
+      <Footer />
     </ScreenContainer>
   );
 }
@@ -65,68 +62,4 @@ const LogoContainer = styled.div`
   }
 `;
 
-const PerguntaAberta = styled.div`
-  width: 300px;
-  margin: 12px;
-  padding: 15px;
-  min-height: 100px;
-  background: #ffffd5;
-  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
-  border-radius: 5px;
-  font-family: "Recursive";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 22px;
-  color: #333333;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  img {
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-  }
-`;
 
-const FooterConcluidos = styled.div`
-  width: 100%;
-  min-height: 50px;
-  background-color: #ffffff;
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: "Recursive";
-  font-weight: 400;
-  font-size: 18px;
-  color: #333333;
-  padding: 10px;
-`;
-
-const ContainerBotoes = styled.div`
-  width: 100%;
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  button {
-    width: 90px;
-    font-family: "Recursive";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 14px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    color: #ffffff;
-    background: blue;
-    border-radius: 5px;
-    border: 1px solid blue;
-    padding: 5px;
-  }
-`;
