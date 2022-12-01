44936948137b20b2906824040b221eed510deb83
rsankutty <r.sankutty@gmail.com>
2022-12-01 10:17:24 -0300

    comeco componentizacao

---
+++ src/components/Main.js
@@ -0,0 +1,154 @@
+import logo from "../assets/img/logo.png";
+import seta_virar from "../assets/img/seta_virar.png";
+import seta_play from "../assets/img/seta_play.png";
+import styled from "styled-components";
+import cards from "./Cards";
+
+export default function Main() {
+    console.log(cards)
+    return (
+        <ScreenContainer>
+            <LogoContainer>
+                <img src={logo} alt="logo"></img>
+                <h1>ZapRecall</h1>
+            </LogoContainer>
+            <PerguntaFechada>
+                <p>teste</p>
+                <img src={seta_play} alt="seta-virar"></img>
+            </PerguntaFechada>
+            <PerguntaAberta>
+                <img src={seta_virar} alt="seta-virar"></img>
+            </PerguntaAberta>
+            <PerguntaAberta>
+                um monte de bosta
+                <ContainerBotoes>
+                    <button>teste</button>
+                    <button>merda</button>
+                    <button>bosta fadfa </button>
+                </ContainerBotoes>
+            </PerguntaAberta>
+            <FooterConcluidos>um monte de bosta</FooterConcluidos>
+        </ScreenContainer>
+    );
+}
+
+
+const ScreenContainer = styled.div`
+  background-color: #fb6b6b;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0px;
+  padding: 0px;
+  padding-bottom: 200px;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 40px 0 20px 0;
+  img {
+    width: 52px;
+  }
+  h1 {
+    font-family: "Righteous";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #ffffff;
+    margin-left: 20px;
+  }
+`;
+
+const PerguntaFechada = styled.div`
+width: 300px;
+height: 35px;
+background-color: #FFFFFF;
+margin: 12px;
+padding: 15px;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+  p {
+    font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: #333333;
+  }
+`;
+
+
+const PerguntaAberta = styled.div`
+width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #FFFFD5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+`;
+
+
+const FooterConcluidos = styled.div`
+width: 100%;
+  min-height: 50px;
+  background-color: #FFFFFF;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Recursive';
+  font-weight: 400;
+  font-size: 18px;
+  color: #333333;
+  padding: 10px;
+`;
+
+
+const ContainerBotoes = styled.div`
+width: 100%;
+display: flex;
+width: 100%;
+justify-content: space-between;
+button {
+  width: 90px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #FFFFFF;
+  background: blue;
+  border-radius: 5px;
+  border: 1px solid blue;
+  padding:5px;
+}
+`;
